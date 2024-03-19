47aa365af0b241dd74c3df70dbf826acdbd99e9c
Aaron Fang <7587244+aaronjfang@users.noreply.github.com>
2024-03-19 11:28:05 -0700

    model verified against paper results

--- run_underactuated_model.m
+++ run_underactuated_model.m
@@ -25,7 +25,64 @@ eps_w = 1.0e-6;
 % Gimbal Rate Restriction
 delta_dot_max = 1.0;
 
+
+
 % Initial Conditions
 w_0 = [0.2; -0.3];
 z_0 = [0.5];
 omega_0 = [0; 0; 0];
+
+% Gimbal Limits
+delta_max = 90;
+delta_min = -90;
+
+out = sim("underactuated_model.slx");
+
+%% Plotting
+figure(1001);
+tiledlayout(3, 2);
+nexttile()
+
+linewidth = 1.0;
+
+plot(out.tout, out.z, 'Color', [0 1 0], 'LineWidth', linewidth);
+hold on
+plot(out.tout, out.w(1, :), 'Color', [0 0 1], 'LineWidth', linewidth);
+plot(out.tout, out.w(2, :), 'Color', [1 0 0], 'LineWidth', linewidth);
+xlabel("Time (s)")
+ylabel("W-Z Parameters")
+
+nexttile();
+plot(out.tout, out.omega(1, :), 'Color', [0 0 1], 'LineWidth', linewidth);
+hold on
+plot(out.tout, out.omega(2, :), 'Color', [1 0 0], 'LineWidth', linewidth);
+plot(out.tout, out.omega(3, :), 'Color', [0 1 0], 'LineWidth', linewidth);
+xlabel("Time (s)")
+ylabel("Angular Velocity (rad/s)")
+
+nexttile();
+plot(out.tout, out.delta_dot(1, :), 'Color', [0 0 1], 'LineWidth', linewidth);
+hold on
+plot(out.tout, out.delta_dot(2, :), 'Color', [1 0 0], 'LineWidth', linewidth);
+xlabel("Time (s)")
+ylabel("Gimbal Rate (rad/s)")
+
+nexttile();
+plot(out.tout, rad2deg(out.delta(1, :)), 'Color', [0 0 1], 'LineWidth', linewidth);
+hold on
+plot(out.tout, rad2deg(out.delta(2, :)), 'Color', [1 0 0], 'LineWidth', linewidth);
+xlabel("Time (s)")
+ylabel("Gimbal Angle (deg)")
+
+nexttile();
+plot(out.tout, -out.M_int(1, :), 'Color', [0 0 1], 'LineWidth', linewidth);
+hold on
+plot(out.tout, -out.M_int(2, :), 'Color', [1 0 0], 'LineWidth', linewidth);
+plot(out.tout, -out.M_int(3, :), 'Color', [0 1 0], 'LineWidth', linewidth);
+xlabel("Time (s)")
+ylabel("Torque (N-m)")
+
+nexttile();
+plot(out.tout, out.det_D_jac, 'Color', [0 0 1], 'LineWidth', linewidth);
+xlabel("Time (s)")
+ylabel("|det(D)|")
